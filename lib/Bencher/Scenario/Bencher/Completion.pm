78bd04707ea7bf995141c1136c76289aff43f0e7
perlancar (@pc-office) <perlancar@gmail.com>
2016-01-06 17:58:47 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/Bencher/Completion.pm
@@ -0,0 +1,38 @@
+package Bencher::Scenario::Bencher::Completion;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Bencher::ScenarioUtil::Completion qw(make_completion_participant);
+
+our $scenario = {
+    summary => 'Benchmark completion response time, to monitor regression',
+    modules => {
+        'App::lcpan' => 0,
+    },
+    participants => [
+        make_completion_participant(
+            name=>'optname_common_help',
+            cmdline=>"bencher --hel^",
+        ),
+        make_completion_participant(
+            name=>'optname_common_version',
+            cmdline=>"bencher --vers^",
+        ),
+        make_completion_participant(
+            name=>'optname_action',
+            cmdline=>"bencher --acti^",
+        ),
+        make_completion_participant(
+            name=>'optval_action',
+            cmdline=>"bencher --action ^",
+        ),
+    ],
+};
+
+1;
+# ABSTRACT:
---
+++ lib/Bencher/Scenarios/Bencher.pm
@@ -0,0 +1,11 @@
+package Bencher::Scenarios::Bencher;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: A collection of scenarios to benchmark Bencher
+
+=head1 SEE ALSO
+
+L<Bencher>
